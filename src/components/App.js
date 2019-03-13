9ad3f5bcf89704b0399bb51b659fb6428b16a9e7
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-13 08:28:06 -0700

    basic app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = { resource: 'posts'};
+
   render() {
     return (
       <div>
-        
+        <div>
+          <button onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
+          <button onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
+        </div>
+        {this.state.resource}
       </div>
     );
   }
