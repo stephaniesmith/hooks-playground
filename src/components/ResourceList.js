1103d921f4b61f44743c9713f57895a90486aad7
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-14 08:38:26 -0700

    hooks are lit

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,26 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const useResources = resource => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(
-    () => {
-    (async() => {
-      const { data } = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-  
-      setResources(data)
-    })(resource);
-    },
-    [resource]
-  );
-  
-  return resources;
-}
+import React from 'react';
+import useResources from './useResources';
 
 const ResourceList = ({ resource }) => {
-
-  useResources(resource);
+  const resources = useResources(resource);
 
   return (
     <ul>
---
+++ src/components/UserList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useResources from './useResources';
+
+const UserList = () => {
+  const users = useResources('users');
+
+  return (
+    <ul>
+      {users.map(({ id, name }) => <li key={id}>{name}</li>)}
+    </ul>
+  );
+};
+
+export default UserList;
